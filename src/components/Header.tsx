3dc7fef3f57d27109bfe67e23aa10c7191a9cf2e
ilya <lizarthej@gmail.com>
2025-03-19 01:41:57 +0200

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,9 @@
-'use client'
-import React, { useEffect, useState }  from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Logo2 from "./img/Logo2.svg";
-import Image from 'next/image';
+import Image from "next/image";
 import { useThemeStore } from "../zustand/themeStore";
 import { getCookie } from "cookies-next";
-
 
 type Props = {
   props?: string;
@@ -17,7 +16,7 @@ const Header: React.FC<Props> = () => {
   useEffect(() => {
     const savedTheme = (getCookie("theme") as "light" | "dark") || "light";
     setTheme(savedTheme);
-    setIsMounted(true); 
+    setIsMounted(true);
   }, [setTheme]);
 
   // Ждём, пока тема загрузится
@@ -26,21 +25,26 @@ const Header: React.FC<Props> = () => {
   }
 
   return (
-    <div className="w-full bg-amber-50">
-      <div className="flex headerglobal justify-between items-center ">
-        <div className=" gap-2 flex w-auto p-3 text-2xl font-bold text-black  ">
-          
+    <div className="w-full ">
+      <div className="flex headerglobal justify-between items-center   ">
+        <div className=" gap-2 flex w-auto p-3 text-2xl font-bold   ">
           <Image src={Logo2} alt="Логотип" className="w-6 " />
           <h1>BShose</h1>
-
         </div>
-        <div className="flex gap-10 w-[50%] bg-amber-800">
-          <div className=" ">
-            <h1>Header</h1>
-            <h1>Header</h1>
-            <button onClick={toggleTheme} className="p-2 bg-gray-200 rounded">
-            {theme === "light" ? "🌙 " : "☀️ "}
-          </button>
+        <div className="flex  justify-center items-center gap-10 w-[60%] ">
+          <div className="flex items-center   gap-10 text-2xl font-medium">
+            <h1>Nike</h1>
+            <h1>Adidas</h1>
+            <h1>Puma</h1>
+            <h1>New Balance</h1>
+            <button
+              onClick={toggleTheme}
+              className={`p-2 row-auto rounded transition-colors duration-300 ${
+                theme === "light" ? "bg-black text-white" : "bg-white text-black"
+              }`}
+            >
+              {theme === "light" ? `🌙` : "☀️ "}
+            </button>
           </div>
         </div>
       </div>
